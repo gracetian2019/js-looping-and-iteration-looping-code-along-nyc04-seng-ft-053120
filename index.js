6dfc3a004e70a71f827c557d4eb341e308b3839c
gracetian2019 <gracetian4ever@gmail.com>
2020-08-01 19:37:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let name = ['Lisa', 'Kaitlin', 'Jan'];
+let surprise = "surprise";
+let message = [];
+
+function writeCards(name, surprise) {
+    for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return message;
+}
+
+function countDown() {
+    for (let i = 10; i >= 0; i--) {
+        console.log(i);
+    }
+}
